bc70aa33973f0ae06408b989026b90210742e22e
jugehlen <60777239+jugehlen@users.noreply.github.com>
2024-07-01 09:11:01 -0300

    Create variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+# Obtendo números do usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Realizando operações
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+# Imprimindo resultados
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
